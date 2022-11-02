f1267127adfb52f1545f5dde3b885b70255bb65b
bensalemchaima <chaima.bensalem@esprit.tn>
2022-11-02 02:15:28 +0100

    mockito added to project

---
+++ src/main/java/tn/esprit/rh/achat/services/IUserService.java
@@ -0,0 +1,17 @@
+package tn.esprit.rh.achat.services;
+
+import java.util.List;
+
+import tn.esprit.rh.achat.entities.User;
+
+
+public interface IUserService { 
+	 
+	List<User> retrieveAllUsers(); 
+	User addUser(User u);
+	void deleteUser(String id);
+	User updateUser(User u);
+	User retrieveUser(String id);
+
+} 
+ 
